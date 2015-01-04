bfe561b4ed0e93ef3b0f123709e12e756d712196
Κώστας Λουπασάκης <loupax@Kostass-MacBook-Pro.local>
2015-01-04 18:31:20 +0200

    Testing for messages that shouldn't appear while switching from one view to another

--- client/scripts/messages-of-user.js
+++ client/scripts/messages-of-user.js
@@ -3,7 +3,7 @@ var isReady;
 Template.UserMessages.helpers({
 	messages: function(){
 		isReady = Meteor.subscribe('MessagesOfUser');
-		return SignsCollection.find({},{
+		return SignsCollection.find({poster_id: Meteor.user()?Meteor.user()._id:-1},{
 			sort:{
   				when:-1
   			}
