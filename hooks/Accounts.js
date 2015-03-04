a0030ba754ea70c6805c65e4359fb96dda9122ad
Kostas Loupasakis <loupax@gmail.com>
2015-03-04 23:54:15 +0200

    Removed problematic hook

--- hooks/Accounts.js
+++
@@ -1,7 +0,0 @@
-/**
- * Extending the user creation functionality
- */
-Accounts.onCreateUser(function(options, user){
-    user.username = user.emails[0].address.replace(/@|\./g, '_');
-    return user;
-});
