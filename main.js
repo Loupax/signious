2cf2cf6e9b13d3f09a08c2f409297dad1d0b746b
Kostas Loupasakis <loupax@gmail.com>
2015-05-12 20:31:34 +0300

    Added SEO package

---
+++ main.js
@@ -0,0 +1,10 @@
+Meteor.startup(function() {
+    if(Meteor.isClient){
+        return SEO.config({
+            title: 'Signious - leaving your sign to the world',
+            meta: {
+                'description': 'Signious allows you to leave messages in the real world for other people to see'
+            }
+        });
+    }
+});
