4e5ea1360d3e33e6dff78cc21dd89dc6f892d102
Kostas Loupasakis <loupax@gmail.com>
2015-05-18 20:30:22 +0300

    Added safe fallback for users that deny geolocation

--- client/main.js
+++ client/main.js
@@ -3,23 +3,36 @@
 // denies Geolocation access. We need that for 2 reasons.
 // A: Make the app usable even without geolocation
 // B: Make the app crawlable even without geolocation
-var INITIAL_LOCATION = new Location({latitude: 90, longitude: 0, altitude: 0});
+var INITIAL_LOCATION = new Location();
 Session.set('lastKnownLocation', INITIAL_LOCATION);
 
 Signious = {
+	updateLocationFromIP: function updateLocationFromIP(){
+		Meteor.call('myGeoIPLocation',function(err, location){
+			if(!err){
+				Signious.geolocation.set(location);
+				Session.set('lastKnownLocation', new Location(Signious.geolocation.lastKnownLocation));
+			}
+		});
+	},
 	geolocation: {
 		onChange: function(location){
-			//console.log('Location!', location);
 			Signious.geolocation.lastKnownLocation.set(location.coords);
 			if(!Location.prototype.isIdentical.call(location, Signious.geolocation.lastKnownLocation)){
 				Session.set('lastKnownLocation', new Location(Signious.geolocation.lastKnownLocation));
 			}
-			
 		},
 		onError: function(error){
 			switch(error.code){
 				// PERMISSION_DENIED
 				case 1:
+					if(Utilities.geolocation !== geolocationFallback){
+						// User denied geolocation... Switching to IP geolocation instead
+						// just to keep the app running
+						Utilities.clearWatch(Signious.geolocation.LOCATION_WATCH_ID);
+						Utilities.geolocation = geolocationFallback;
+						Signious.geolocation.LOCATION_WATCH_ID = Utilities.geolocation.watchPosition(Signious.geolocation.onChange, Signious.geolocation.onError, Signious.geolocation.options);
+					}
 				break;
 				// POSITION_UNAVAILABLE
 				case 2:
@@ -33,14 +46,55 @@ Signious = {
 		options: {
 		  enableHighAccuracy: false,
 		  timeout: 5000,
-		  maximumAge: 0
+		  maximumAge: 1000 * 60
 		},
 
 		lastKnownLocation: INITIAL_LOCATION,
 		LOCATION_WATCH_ID: undefined
 	}
 };
-
-if(navigator.geolocation) {
-	Signious.geolocation.LOCATION_WATCH_ID = navigator.geolocation.watchPosition(Signious.geolocation.onChange, Signious.geolocation.onError, Signious.geolocation.options);
+Utilities = {};
+// We keep the fallback in a variable, to allow switching on the fly later
+// in the case the user denies us geolocation. Not as accurate but still better
+// than having an unusable app
+var geolocationFallback = {
+	watchPosition: function watchPosition(onChange, onError, options){
+		var o = _.extend({timeout:5000, maximumAge:1000 * 60},options);
+		return Meteor.setInterval(function(){
+			Utilities.geolocation.getCurrentPosition(function(data){
+				onChange(data);
+			}, function(error){
+				onError(error);
+			});
+		}, o.maximumAge);
+	},
+	clearWatch: function clearWatch(watchId){
+		Meteor.clearInterval(watchId);
+	},
+	getCurrentPosition: function getCurrentPosition(success,error){
+		Meteor.call('myGeoIPLocation',function(err, location){
+			var _loc = {
+				coords: {
+					accuracy: null,
+					altitude: null,
+					altitudeAccuracy: null,
+					heading: null,
+					latitude: location.latitude,
+					longitude: location.longitude,
+					speed: null
+				},
+				timestamp: new Date().getTime()
+			};
+			if(err)
+				error(err);
+			else
+				success(_loc);
+		});
+	}
+};
+if(!navigator.geolocation) {
+	Utilities.geolocation = geolocationFallback;
+}else{
+	Utilities.geolocation = navigator.geolocation;
 }
+Signious.geolocation.LOCATION_WATCH_ID = Utilities.geolocation.watchPosition(Signious.geolocation.onChange, Signious.geolocation.onError, Signious.geolocation.options);
--- hooks/Accounts.js
+++ hooks/Accounts.js
@@ -17,20 +17,19 @@ if(Meteor.isServer){
 
     Meteor.methods({
         'generateUsername': function(){
-            var exec = Npm.require('child_process').exec, os = Npm.require('os');
-            var adjective = '';
-            var noun = '';
-            var shuffler = os.platform() === 'darwin'?'gshuf':'shuf';
-            var generateAdjective = function(callback){
-                return exec("cat /usr/share/dict/words | grep 'a$\\|ing$\\|ly$\\|ary$\\|ful$\\|ic$\\|ical$\\|ish$\\|less$\\|like$\\|ly$\\|ous$\\|y$' | "+shuffler+" -n 1", callback);
-            }
-            var generateNoun = function(callback){
-                return exec("cat /usr/share/dict/words | grep -v 'ing$\\|ly$\\|ary$\\|ful$\\|ic$\\|ical$\\|ish$\\|less$\\|like$\\|ly$\\|ous$\\|y$' | "+shuffler+" -n 1", callback);
-            }
-            var a = Meteor.wrapAsync(generateAdjective)().trim();
-            var b = Meteor.wrapAsync(generateNoun)().trim();
-            //console.log(adjective, noun);
-            return [a,b].map(function(a){return a.toLowerCase();}).join('_');
+            console.log(this.connection);
+            var exec = Npm.require('child_process').exec,
+                os = Npm.require('os'),
+                // Dev is osx, prod is Ubuntu. Deal with it
+                shuffler = os.platform() === 'darwin'?'gshuf':'shuf';
+
+            var pickAdjective = Meteor.wrapAsync(function(callback){
+                exec("cat /usr/share/dict/words | grep 'love$\\|a$\\|ing$\\|ly$\\|ary$\\|ful$\\|ic$\\|ical$\\|ish$\\|less$\\|like$\\|ly$\\|ous$\\|y$' | "+shuffler+" -n 1", callback);
+            });
+            var pickNoun = Meteor.wrapAsync(function(callback){
+                exec("cat /usr/share/dict/words | grep -v 'ing$\\|ly$\\|ary$\\|ful$\\|ic$\\|ical$\\|ish$\\|less$\\|like$\\|ly$\\|ous$\\|y$' | "+shuffler+" -n 1", callback);
+            });
+            return [pickAdjective().trim(),pickNoun().trim()].map(function(a){return a.toLowerCase();}).join('_');
         }
     })
 }
