82593d7a344cbd72cdfd1a50b38387dcd287e17c
Kostas Loupasakis <loupax@gmail.com>
2015-05-24 21:53:46 +0300

    Deleted the old preparations, fixed the publication that fetches the specific messages to accomodate with the private

--- packages/sign/publications/public_sign_publication.js
+++ packages/sign/publications/public_sign_publication.js
@@ -1,6 +1,6 @@
 Meteor.publish('SpecificPublicSign', function(_id){
    return SignsCollection.find({
       _id: _id,
-      $or: [{'is_deleted': true, poster_id: this.userId}, {is_private:false}]
+      $or: [{'is_deleted': true, poster_id: this.userId}, {'is_private': true, poster_id: this.userId}]
    }, {limit:1});
 });
